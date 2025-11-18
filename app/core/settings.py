20bc639e3a88e8b684492975cda9f60b3b94de87
dav4k2 <hoangthanhphongsn04@gmail.com>
2025-11-18 20:23:52 +0700

    ghi chu trong READ.ME

--- app/core/settings.py
+++ app/core/settings.py
@@ -8,7 +8,9 @@ class Settings(BaseSettings):
     # URL kết nối đến PostgreSQL
     # Ví dụ: "postgresql+psycopg2://user:password@localhost:5432/mydb"
     DATABASE_URL: str = "postgresql://postgres:181004@localhost:5432/cooking_db"    
+    
     # Khóa bí mật để tạo JWT
+    
     SECRET_KEY: str = os.getenv("SECRET_KEY", "664c34290d28294b9afedd198f8ec91219177defc65bf8ab65a051a25e00f2ba")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 ngày
